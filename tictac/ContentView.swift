f7cb790121de739d6e52ccda8fb51e0bfbac5c7b
Arnav Doshi <arnavdoshi47@gmail.com>
2021-10-31 14:20:51 -0700

    Initial Commit

---
+++ tictac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tictac
+//
+//  Created by Arnav Doshi on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tictac/tictacApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictacApp.swift
+//  tictac
+//
+//  Created by Arnav Doshi on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct tictacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
